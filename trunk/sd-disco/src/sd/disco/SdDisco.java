47f29cc8110fcac32a6846f568c129a4b1f13dcf
f3l1nux@gmail.com <f3l1nux@gmail.com@f00cf526-d9a8-7a3c-703e-2bea5a9d4ef3>
2012-03-09 16:33:30 +0000

    Postagem do código que você me enviou por e-mail.

--- trunk/sd-disco/src/sd/disco/SdDisco.java
+++ trunk/sd-disco/src/sd/disco/SdDisco.java
@@ -1,19 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package sd.disco;
+package sd.disco; 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.io.IOException;
+import java.net.SocketException;
 
-/**
- *
- * @author felipe
- */
 public class SdDisco {
+       public static void main(String args[]) {
+       DatagramSocket s = null;
+       try {
+           s = new DatagramSocket(6789);
+           byte[] buffer = new byte[1000];
+           System.out.println("\n\n*** Servidor aguardando request");
+           DatagramPacket req = new DatagramPacket(buffer, buffer.length);
+           s.receive(req);
+           System.out.println("*** Request recebido de: " +
+req.getAddress()+":"+req.getPort());
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+           // envia resposta
+           DatagramPacket resp = new DatagramPacket(req.getData(),
+req.getLength(),
+                   req.getAddress(), req.getPort());
+           s.send(resp);
+       } catch (SocketException e) {
+           System.out.println("Erro de socket: " + e.getMessage());
+       } catch (IOException e) {
+           System.out.println("Erro envio/recepcao pacote: " + e.getMessage());
+       } finally {
+           if (s != null) s.close();
+       }
+   }
 }
